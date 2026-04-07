a713aad23d4a74acd588822c637e5dfc23c164b1
Kostiantyn Didovych <didovych.k.v@gmail.com>
2026-04-07 11:07:40 +0300

    2.2 Simple numbers (for, tag)

---
+++ src/tasks/2_2/simple_numbers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(() => {
+    simpleNumbers: for (let i = 1; i <= 100; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                continue simpleNumbers;
+            }
+        }
+        console.log(i);
+    }
+})();
