da31d6b644bd4019450cd1740db4e469ee0128c9
Kostiantyn Didovych <didovych.k.v@gmail.com>
2026-04-08 15:12:10 +0300

    2.4 Father twice older than son (jsDocs, restructure)

--- src/tasks/2_4/twice_older.js
+++ src/tasks/2_4/twice_older.js
@@ -4,7 +4,31 @@
     let father = prompt('Enter how old is Father');
     let son = prompt('Enter how old is Son');
     let years = whenTwiceOlder(father, son);
+    outputYears(years);
+})();
 
+/**
+ * Calculate amount of years (negative if action was in past) when father's age was twice as son's
+ *
+ * @param {Number} father 
+ * @param {Number} son 
+ * @returns Number
+ */
+function whenTwiceOlder(father, son) {
+    let difference = father - son;
+    if (father < 15 || son < 0 || difference < 15) {
+        throw new Error('Неверние входние данние');
+    }
+    return father - 2 * son;
+}
+
+/**
+ * Output years result in text format to console
+ *
+ * @param {Number} years
+ * @returns void
+ */
+function outputYears(years) {
     if (years === 0) {
         console.log('Отец сейчас вдвое старше сына');
         return;
@@ -27,12 +51,4 @@
     } else {
         console.log(`Отец был вдвое старше сына ${-years} ${yearsWord} назад`);
     }
-})();
-
-function whenTwiceOlder(father, son) {
-    let difference = father - son;
-    if (father < 15 || son < 0 || difference < 15) {
-        throw new Error('Неверние входние данние');
-    }
-    return father - 2 * son;
 }
